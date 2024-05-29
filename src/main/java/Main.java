ca9532626663e2e6659608ffc6c924a42c4fb950
JDjakubd <162459443+JDjakubd@users.noreply.github.com>
2024-05-29 12:45:33 +0000

    6.3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,7 @@ Poniższe zadania będą się sprowadzały do modyfikacji bazowego kodu. Proces 
 //Niepoprawny wiek – gdy jest mniejszy od 0 lub większy niż 100. Niepoprawna data urodzenia – gdy nie jest zapisana w formacie DD-MM-YYYY, np. 28-02-2023.
 
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.io.IOException;
 
@@ -20,6 +21,7 @@ class WrongStudentName extends Exception { }
 
 class WrongAge extends Exception { }
 class WrongDateOfBirth extends Exception { }
+class WrongMenuChoice extends Exception { }
 
 class Main {
     public static Scanner scan = new Scanner(System.in);
@@ -42,17 +44,26 @@ class Main {
                 System.out.println("Błędny wiek studenta!");
             }catch(WrongDateOfBirth e) {
                 System.out.println("Błędna data urodzenia studenta!");
+            }catch(WrongMenuChoice e) {
+                System.out.println("Błędny wybór z menu!");
+                scan.nextLine();
             }
         }
     }
 
-    public static int menu() {
+    public static int menu() throws WrongMenuChoice {
         System.out.println("Wciśnij:");
         System.out.println("1 - aby dodać studenta");
         System.out.println("2 - aby wypisać wszystkich studentów");
         System.out.println("3 - aby wyszukać studenta po imieniu");
         System.out.println("0 - aby wyjść z programu");
-        return scan.nextInt();
+        try {
+            int choice = scan.nextInt();
+            scan.nextLine();
+            return choice;
+        } catch(InputMismatchException e) {
+            throw new WrongMenuChoice();
+        }
     }
 
     public static String ReadName() throws WrongStudentName {
